d52f9ad653ee1bded479972c66bfff7006ab1251
Francesco Riva <friva.job@outlook.com>
2022-04-13 17:22:08 +0200

    first commit

---
+++ src/main/java/servlet/NavigationServlet.java
@@ -0,0 +1,148 @@
+package servlet;
+
+import jakarta.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NavigationServlet
+ */
+@WebServlet("/NavigationServlet")
+public class NavigationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	private static final String USER = "sa";
+	private static final String PWD = "Riva96_shared_db";
+	private static final String DRIVER_CLASS = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+	private static final String DB_URL = "jdbc:sqlserver://localhost:1433;databaseName=examDB;encrypt=true;trustServerCertificate=true;";
+    
+	private static Connection conn;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NavigationServlet() {
+        super();
+    }
+    
+    public void init() throws ServletException {
+    	try {
+			Class.forName(DRIVER_CLASS);
+			
+		    Properties connectionProps = new Properties();
+		    connectionProps.put("user", USER);
+		    connectionProps.put("password", PWD);
+	
+	        conn = DriverManager.getConnection(DB_URL, connectionProps);
+		    
+		    //System.out.println("User \"" + USER + "\" connected to database.");
+    	
+    	} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		String email = (String) request.getAttribute("email");
+		String pwd = (String) request.getAttribute("password");
+		
+		if (request.getParameter("newMail") != null)
+			request.setAttribute("content", getHtmlForNewMail(email, pwd));
+		else if (request.getParameter("inbox") != null)
+			request.setAttribute("content", getHtmlForInbox(email));
+		else if (request.getParameter("sent") != null)
+			request.setAttribute("content", getHtmlForSent(email));
+		
+		request.getRequestDispatcher("home.jsp").forward(request, response);
+	}
+
+	private String getHtmlForInbox(String email) {
+		try (Statement st = conn.createStatement()) {
+			ResultSet sqlRes = st.executeQuery(
+				"SELECT * FROM mail "
+				+ "WHERE receiver='" + email + "'"
+				+ "ORDER BY [time] DESC"
+			);
+			
+			StringBuilder output = new StringBuilder();
+			output.append("<div>\r\n");
+			
+			while (sqlRes.next()) {
+				output.append("<div>\r\n<span style=\"color:grey;\">\r\n");
+				output.append("FROM:&emsp;" + sqlRes.getString(1) + "&emsp;&emsp;AT:&emsp;" + sqlRes.getString(5) + "\r\n");
+				output.append("</span>\r\n");
+				output.append("<br><b>" + sqlRes.getString(3) + "</b>\r\n");
+				output.append("<br><b>" + sqlRes.getString(4) + "</b>\r\n");
+				output.append("</div>\r\n");
+				
+				output.append("<hr style=\"border-top: 2px solid black;\">\r\n");
+			}
+			
+			output.append("</div>");
+			
+			return output.toString();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return "ERROR IN FETCHING INBOX MAILS!";
+		}
+	}
+	
+	private String getHtmlForNewMail(String email, String pwd) {
+		return 
+			"<form id=\"submitForm\" class=\"form-resize\" action=\"SendMailServlet\" method=\"post\">\r\n"
+			+ "		<input type=\"hidden\" name=\"email\" value=\""+email+"\">\r\n"
+			+ "		<input type=\"hidden\" name=\"password\" value=\""+pwd+"\">\r\n"
+			+ "		<input class=\"single-row-input\" type=\"email\" name=\"receiver\" placeholder=\"Receiver\" required>\r\n"
+			+ "		<input class=\"single-row-input\" type=\"text\"  name=\"subject\" placeholder=\"Subject\" required>\r\n"
+			+ "		<textarea class=\"textarea-input\" name=\"body\" placeholder=\"Body\" wrap=\"hard\" required></textarea>\r\n"
+			+ "		<input type=\"submit\" name=\"sent\" value=\"Send\">\r\n"
+			+ "	</form>";
+	}
+	
+	private String getHtmlForSent(String email) {
+		try (Statement st = conn.createStatement()) {
+			ResultSet sqlRes = st.executeQuery(
+				"SELECT * FROM mail "
+				+ "WHERE sender='" + email + "'"
+				+ "ORDER BY [time] DESC"
+			);
+			
+			StringBuilder output = new StringBuilder();
+			output.append("<div>\r\n");
+			
+			while (sqlRes.next()) {
+				output.append("<div>\r\n<span style=\"color:grey;\">\r\n");
+				output.append("TO:&emsp;" + sqlRes.getString(2) + "&emsp;&emsp;AT:&emsp;" + sqlRes.getString(5) + "\r\n");
+				output.append("</span>\r\n");
+				output.append("<br><b>" + sqlRes.getString(3) + "</b>\r\n");
+				output.append("<br><b>" + sqlRes.getString(4) + "</b>\r\n");
+				output.append("</div>\r\n");
+				
+				output.append("<hr style=\"border-top: 2px solid black;\">\r\n");
+			}
+			
+			output.append("</div>");
+			
+			return output.toString();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return "ERROR IN FETCHING INBOX MAILS!";
+		}
+	}
+}
