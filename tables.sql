0ffc1f9ad120a365d8deb6143e2fc53c111faabf
tane994 <andres.tanesini94@gmail.com>
2022-05-06 16:36:11 +0200

    table for sql

---
+++ tables.sql
@@ -0,0 +1,17 @@
+CREATE TABLE users (
+	name varchar(50)  NOT NULL,
+	surname varchar(50) NOT NULL,
+	email varchar(50) NOT NULL,
+	password varchar(50) NOT NULL,
+	CONSTRAINT user_PK PRIMARY KEY (email)
+);
+
+CREATE TABLE mail (
+	sender varchar(50) NOT NULL,
+	receiver varchar(50) NOT NULL,
+	subject varchar(100) NULL,
+	body text NOT NULL,
+	datetime timestamp NOT NULL,
+	CONSTRAINT mail_FK FOREIGN KEY (sender) REFERENCES users (email),
+	CONSTRAINT mail_FK_1 FOREIGN KEY (receiver) REFERENCES users(email)
+);
